c7571781b5b5adcd3ba5fb7911ec3861b8a93c92
fyz <36511274+yafyz@users.noreply.github.com>
2023-12-30 20:42:10 +0100

    Preventively resync random on new level

---
+++ CustomBoomboxTracks/Patches/RoundManager_GenerateNewLevelClientRpc.cs
@@ -0,0 +1,15 @@
+﻿using CustomBoomboxTracks.Managers;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomBoomboxTracks.Patches {
+
+    [HarmonyPatch(typeof(RoundManager), "GenerateNewLevelClientRpc")]
+    internal class RoundManager_GenerateNewLevelClientRpc {
+        static void Postfix(RoundManager __instance) {
+            RandomSyncManager.ResyncRandomSeed(__instance.playersManager.randomMapSeed);
+        }
+    }
+}
